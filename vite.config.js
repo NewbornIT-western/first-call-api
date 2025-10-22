8b006025f0bbe474e398e25e043d1a10ca0cef10
Truong Duy <truongduy.arg@gmail.com>
2025-10-22 21:56:24 +0700

    fixxx

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/first-call-api/' : '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        assetFileNames: 'assets/[name].[hash].[ext]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js'
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  }
+  base: "/first-call-api",
 })
