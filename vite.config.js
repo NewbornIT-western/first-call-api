5fd9dc0d32c4b5478ab650f906c3247aba44718b
Truong Duy <truongduy.arg@gmail.com>
2025-10-22 21:05:35 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/first-call-api/'
+  base: '/first-call-api/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
